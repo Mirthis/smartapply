10288334b4298c206905a462b4f5c468011f6db3
mirthis@gmail.com <mirthis@gmail.com>
2023-07-10 14:48:26 +0100

    add Layout to each page instead of Head tags, refactor imports

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,19 @@
-/** @type {import("prettier").Config} */
+// /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    "@trivago/prettier-plugin-sort-imports",
+  ],
+  importOrder: [
+    "^react(.*)$",
+    "^next(.*)$",
+    "^~/utils/(.*)$",
+    "^~/components(.*)$",
+    "^[~/]",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
 
 module.exports = config;
