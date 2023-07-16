46d190410bd1837d96446a906baa31a2d7b8484a
mirthis@gmail.com <mirthis@gmail.com>
2023-07-16 10:53:04 +0100

    remove profile route, update build command to run migrate deploy

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,11 @@
+import { createTRPCRouter } from "~/server/api/trpc";
+
 import { applicantRouter } from "./routers/applicant";
 import { applicationRouter } from "./routers/application";
 import { contactRouter } from "./routers/contact";
 import { coverLettersRouter } from "./routers/coverLetters";
 import { interviewRouter } from "./routers/interview";
-import { profileRouter } from "./routers/profile";
 import { testRouter } from "./routers/test";
-
-import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -18,7 +17,6 @@ export const appRouter = createTRPCRouter({
   interview: interviewRouter,
   test: testRouter,
   applicant: applicantRouter,
-  profile: profileRouter,
   contact: contactRouter,
   application: applicationRouter,
 });
