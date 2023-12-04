4b2e4e347ca4189fd43a08fcbcfab2030c0f3995
mirthis@gmail.com <mirthis@gmail.com>
2023-12-04 11:31:29 +0000

    update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,8 +15,8 @@ const AboutPage: NextPage = () => {
       <Title title="Dev Team" type="section" />
       <div className="flex flex-col gap-y-4">
         <p>
-          Concept, design and all the back-end and front-end development by
-          Andrea Cardinale.
+          Concept, design, back-end and front-end development by Andrea
+          Cardinale.
         </p>
         <p>
           You can visit my portfolio site for some more info about me and my
@@ -27,15 +27,15 @@ const AboutPage: NextPage = () => {
             href="https://www.andreacardinale.me/"
             className="w-full sm:w-48"
           >
-            <button className="btn-outline btn-primary btn-sm btn w-full ">
-              <UserCircleIcon className="mr-2 h-6 w-6" />
-              Visit Portfolio
+            <button className="flex space-x-2 btn-outline btn-primary  btn w-full ">
+              <UserCircleIcon className="h-6 w-6" />
+              <p>Portfolio</p>
             </button>
           </Link>
           <Link href="/contact" className="w-full sm:w-48">
-            <button className="btn-outline btn-primary btn-sm btn w-full ">
-              <InboxArrowDownIcon className="mr-2 h-6 w-6" />
-              Conact Form
+            <button className="flex space-x-2  btn-outline btn-primary  btn w-full">
+              <InboxArrowDownIcon className="h-6 w-6" />
+              <p>Contact Me</p>
             </button>
           </Link>
         </div>
