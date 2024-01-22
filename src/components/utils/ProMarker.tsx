fd62237337afc106cebe632bea07a4827fea02fa
mirthis@gmail.com <mirthis@gmail.com>
2024-01-22 15:39:28 +0000

    remove heroicon

--- src/components/utils/ProMarker.tsx
+++ src/components/utils/ProMarker.tsx
@@ -1,11 +1,11 @@
-import { LockClosedIcon } from "@heroicons/react/24/solid";
+import { Lock } from "lucide-react";
 
 import Link from "next/link";
 
 const ProMarker = () => {
   return (
     <Link href="/upgrade" className="flex items-center link-accent">
-      <LockClosedIcon className="w-4 h-4 mr-2" />
+      <Lock className="w-4 h-4 mr-2" />
       Pro
     </Link>
   );
